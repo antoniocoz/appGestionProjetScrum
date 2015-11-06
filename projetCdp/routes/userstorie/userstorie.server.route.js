2306b6412a92cb6a4823eaa979c131d1abdcf70d
antoniocoz <cvantonio.07@gmail.com>
2015-11-07 00:29:29 +0100

    modification de route

---
+++ projetCdp/routes/userstorie/userstorie.server.route.js
@@ -0,0 +1,45 @@
+
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var US = mongoose.model('US');
+
+router.get('/userStories/:backlog', function(req, res) {
+    var query = {"backlog": req.params.backlog};
+    US.find(query, function(err, doc) {
+      if (err) { return next(err); }
+        res.json(doc);
+    });
+});
+
+router.post('/userStories/:backlog', function(req, res, next) {
+  var us = new US(req.body);
+  us.backlog = req.backlog;
+
+  us.save(function(err, us){
+    if(err){ return next(err); }
+    res.json(us);
+  });
+});
+
+router.delete('/userStories/:userStory', function(req, res) {
+	US.remove({_id: req.params.userStory}, function(err, doc) {
+    if (err) { return next(err); }
+      res.json(doc);
+    });
+});
+
+//update one US
+router.put('/userStories/:userStory', function(req, res) {
+  var body = req.body.body;
+  var priority = req.body.priority;
+  var difficulty = req.body.difficulty;
+  
+  var query = {"_id":req.params.userStory};
+
+      US.findOneAndUpdate(query, {body: body, priority: priority, difficulty: difficulty}, { 'new': true }, function(doc) {
+        res.json(doc);  
+      });
+});
+
+module.exports = router;

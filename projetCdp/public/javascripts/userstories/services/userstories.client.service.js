e9198c90d32aa99f084c811dc26ebef8bd2fdf19
berguiz <bertrand.guillozet@etu.u-bordeaux1.fr>
2015-11-07 16:53:00 +0100

    Creation des fonctions du service des user stories (createUS et updateUS) avec recuperation de l'id du backlog parent

--- projetCdp/public/javascripts/userstories/services/userstories.client.service.js
+++ projetCdp/public/javascripts/userstories/services/userstories.client.service.js
@@ -1,5 +1,14 @@
 angular.module('userstories').factory('userStories', ['$http', function($http){
   var o = {userStories: []};
+  var idBl;
+
+  o.setIdBl = function(id) {
+    idBl = id;
+  };
+
+  o.getIdBl = function(id) {
+    return idBl;
+  };
 
   o.getAll = function(idBL) {
     return $http.get('/userStories/' + idBL).success(function(data){
@@ -12,6 +21,18 @@ angular.module('userstories').factory('userStories', ['$http', function($http){
 		o.getAll(idBL);
 	  });
 	};
+
+  o.updateUS = function(idBL, id, userstory) {
+    return $http.put('/userStories/' + id, userstory).success(function(response){
+    o.getAll(idBL);
+    });
+  };
+
+  o.createUS = function(userstory) {
+    return $http.post('/userStories', userstory).success(function(data){
+    o.userStories.push(data);
+    });
+  };
  
   return o;
 }]);

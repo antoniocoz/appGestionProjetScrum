40390f2d0980c77b50d0891fbd9d7af6a85b9cf6
antoniocoz <cvantonio.07@gmail.com>
2015-11-07 21:17:04 +0100

    app

--- projetCdp/app.js
+++ projetCdp/app.js
@@ -2,9 +2,9 @@
 var mongoose = require('mongoose');
 require('./models/Backlogs');
 require('./models/Userstories');
-mongoose.connect('mongodb://localhost/cdp');
+//mongoose.connect('mongodb://localhost/cdp');
 // connextion a la base de données online (mongolab)
-//mongoose.connect('mongodb://scrum:developpement@ds047458.mongolab.com:47458/projetscrumdev');
+mongoose.connect('mongodb://scrum:developpement@ds047458.mongolab.com:47458/projetscrumdev');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');

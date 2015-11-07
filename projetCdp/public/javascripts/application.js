5df513b2560900a91a6574333457c5d00e68facb
berguiz <bertrand.guillozet@etu.u-bordeaux1.fr>
2015-11-07 16:47:16 +0100

    Modification de l'etat userStories de l'application

--- projetCdp/public/javascripts/application.js
+++ projetCdp/public/javascripts/application.js
@@ -21,13 +21,13 @@ function($stateProvider, $urlRouterProvider) {
 	  }
     })
 	.state('userStories', {
-	  url: '/userStories/{id}',
+	  url: '/userStories/:id',
 	  templateUrl: 'javascripts/userstories/views/userstories.client.view.html',
 	  controller: 'USCtrl',
 	  // anytime our backlogs state is entered, we will automatically query all US from a backlog
 	  resolve: {
 		userStoriesPromise: ['$stateParams', 'userStories', function($stateParams, userStories) {
-			console.log($stateParams.id);
+			userStories.setIdBl($stateParams.id);
 		  return userStories.getAll($stateParams.id);
 		}]
 	  }

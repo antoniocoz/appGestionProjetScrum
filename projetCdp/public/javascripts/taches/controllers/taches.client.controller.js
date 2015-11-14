4c70be9efd4a443bf4ba8ca93f7692d631a51d38
antoniocoz <cvantonio.07@gmail.com>
2015-11-14 14:20:27 +0100

    taches client

---
+++ projetCdp/public/javascripts/taches/controllers/taches.client.controller.js
@@ -0,0 +1,54 @@
+//US controller
+angular.module('taches').controller('tacheController', ['$scope','$location','tacheService',
+
+function($scope, $location, tacheService){
+	//On récupère l'ensemble des US via getAll du service
+	$scope.taches = tacheService.taches;
+    $scope.editer=false; 
+	$scope.create = function(){
+	      if($scope.numero === '' || !$scope.description || !$scope.dure) { 
+	      	return; 
+	      }
+		  tacheService.create({
+			numero: $scope.numero,
+			description: $scope.description,
+			delaiplustot:0,
+			delaiplustard:0,
+			dure: $scope.dure,
+			usId: $scope.usId,
+			tacheId:[]
+		  });
+		  $scope.numero = '';
+		  $scope.description = '';
+		  $scope.dure = '';
+		  $location.path('taches/' + $scope.usId);
+	};
+	
+	$scope.delete = function(tache){
+		tacheService.delete(tache._id,tache.usId);
+		//$location.path('taches/' + $scope.usId);
+	};
+	
+	$scope.edit = function(tache){
+          $scope.editer=true;
+          $scope.tache=tache;
+	};
+
+    $scope.findAllByUs = function(usId){
+		  //$scope.tache = {};
+          $scope.taches=tacheService.getAll(usId);
+         // $location.path('taches/edit/' + $scope.tache.description);
+	};
+	$scope.findOne = function(tacheId){
+		  $scope.editer=false;
+          $scope.tache=tacheService.get(tacheId);
+         // $location.path('taches/edit/' + $scope.tache.description);
+	};
+	
+	$scope.update = function(tacheId,tache){
+		//if($scope.body === '' || !$scope.priority || !$scope.difficulty) { return; }
+		tacheService.update(tacheId,tache);
+		//$scope.tache={};
+		$scope.editer=false;
+	};
+}]);
---
+++ projetCdp/public/javascripts/taches/services/taches.client.service.js
@@ -0,0 +1,35 @@
+angular.module('taches').factory('tacheService', ['$http', function($http){
+  var o = {taches: []};
+
+  o.getAll = function(usId) {
+    return $http.get('/taches/' + usId).success(function(data){
+      angular.copy(data, o.taches);
+    });
+  };
+
+  //get one backlog in using an id
+  o.get = function(tacheId) {
+    return $http.get('/tache/' + tacheId).then(function(response){
+        return response;
+    });
+  };
+  o.delete = function(tacheId,usId) {
+	  return $http.delete('/taches/' + tacheId).success(function(response){
+		o.getAll(usId);
+	  });
+	};
+
+  o.update = function(tacheId,tache) {
+    return $http.put('/taches/'+tacheId, tache).success(function(response){
+    o.getAll(tache.usId);
+    });
+  };
+
+  o.create = function(tache) {
+    return $http.post('/taches', tache).success(function(data){
+        o.taches.push(data);
+    });
+  };
+ 
+  return o;
+}]);
